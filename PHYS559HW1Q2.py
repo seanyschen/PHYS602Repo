383f18082d7c7bf92c5f1fda95b9c8285612a33b
seanyschen <85811403+seanyschen@users.noreply.github.com>
2025-03-14 23:02:17 -0400

    Add files via upload

---
+++ PHYS559HW1Q2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 13:32:19 2025
+
+@author: Sean
+"""
+
+import numpy as np
+
+fname='hexagonally_packed_disks.txt'
+data=np.loadtxt(fname,delimiter=',')
+print(data)
